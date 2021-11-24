ee3ad4a8ad3dfb582a0a105d4567ba47316bfe22
Dan Martin <dan@dan-martin.net>
2021-11-24 10:52:55 +0000

    Update null logger to include return types

--- DanDMartin/LoggerAware/Logger/NullLogger.php
+++ DanDMartin/LoggerAware/Logger/NullLogger.php
@@ -5,40 +5,40 @@ use Psr\Log\LoggerInterface as PsrLogger;
 
 class NullLogger implements PsrLogger
 {
-    public function emergency($message, array $context = array())
+    public function emergency($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-    public function alert($message, array $context = array())
+    public function alert($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-	public function critical($message, array $context = array())
+    public function critical($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-	public function error($message, array $context = array())
+    public function error($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-	public function warning($message, array $context = array()) 
+    public function warning($message, array $context = array()) : void
     {
-        return null;
+        return;
     }
-	public function notice($message, array $context = array()) 
+    public function notice($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-    public function info($message, array $context = array()) 
+    public function info($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-    public function debug($message, array $context = array()) 
+    public function debug($message, array $context = array()): void
     {
-        return null;
+        return;
     }
-    public function log($level, $message, array $context = array()) 
+    public function log($level, $message, array $context = array()): void
     {
-        return null;
+        return;
     }
 }
